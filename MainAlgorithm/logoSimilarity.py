09a8f9b84e4c40da544e63e898091228eeb24fc3
ShahilPatel-IITDh <shahilpatel809@gmail.com>
2023-10-15 16:55:12 +0530

    Removed unwanted files and updated few portions of code.

--- MainAlgorithm/logoSimilarity.py
+++ MainAlgorithm/logoSimilarity.py
@@ -1,101 +1,46 @@
+# Various algorithms will be used to compare the similarity of two logos, so that we can check which one will give us most accurate results.
+
+# 1. Mean Squared Error (MSE): MSE measures the average squared difference between pixel values in two images. It's simple to implement but might not work well for complex image comparisons.
+
+import cv2
 import os
-import numpy as np
-from PIL import Image, ImageOps
-from torchvision import transforms
-import torch
-import torch.nn.functional as F
 
-def l2_norm(x):
-    '''L2 Normalization'''
-    if len(x.shape):
-        x = x.reshape((x.shape[0], -1))
-    return F.normalize(x, p=2, dim=1)
+def mse_similarity(image_path1, folder_path):
+    # Load the input image
+    img1 = cv2.imread(image_path1)
 
-def pred_siamese(img_path, model, grayscale=False):
-    img_size = 128
-    mean = [0.5, 0.5, 0.5]
-    std = [0.5, 0.5, 0.5]
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    # Ensure the folder path exists
+    if not os.path.exists(folder_path) or not os.path.isdir(folder_path):
+        return {}
 
-    img_transforms = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize(mean=mean, std=std),
-    ])
+    similarity_scores = {}
 
-    img = Image.open(img_path)
-    img = img.convert("L").convert("RGB") if grayscale else img.convert("RGB")
+    # Resize the input image to the lowest resolution within the folder
+    min_height = img1.shape[0]
+    min_width = img1.shape[1]
 
-    pad_color = 255 if grayscale else (255, 255, 255)
-    img = ImageOps.expand(img, (
-        (max(img.size) - img.size[0]) // 2, (max(img.size) - img.size[1]) // 2,
-        (max(img.size) - img.size[0]) // 2, (max(img.size) - img.size[1]) // 2), fill=pad_color)
+    for filename in os.listdir(folder_path):
+        image_path2 = os.path.join(folder_path, filename)
+        img2 = cv2.imread(image_path2)
 
-    img = img.resize((img_size, img_size))
+        # Ensure img2 is a valid image
+        if img2 is not None:
+            min_height = min(min_height, img2.shape[0])
+            min_width = min(min_width, img2.shape[1)
 
-    with torch.no_grad():
-        img = img_transforms(img)
-        img = img[None, ...].to(device)
-        logo_feat = model.features(img)
-        logo_feat = l2_norm(logo_feat).squeeze(0).cpu().numpy()
+    img1 = cv2.resize(img1, (min_width, min_height))
 
-    return logo_feat
+    # Calculate the Mean Squared Error for each image in the folder
+    for filename in os.listdir(folder_path):
+        image_path2 = os.path.join(folder_path, filename)
+        img2 = cv2.imread(image_path2)
 
-def siamese_inference(model, domain_map, logo_feat_list, file_name_list, shot_path, gt_bbox, t_s, grayscale=False):
-    try:
-        img = Image.open(shot_path)
-        
-    except OSError:
-        print('Screenshot cannot be open')
-        return None, None, None
+        # Ensure img2 is a valid image
+        if img2 is not None:
+            img2 = cv2.resize(img2, (min_width, min_height))
+            mse = ((img1 - img2) ** 2).mean()
 
-    cropped = img.crop((gt_bbox[0], gt_bbox[1], gt_bbox[2], gt_bbox[3]))
-    img_feat = pred_siamese(cropped, model, grayscale=grayscale)
+            if mse < 0.2:  # Adjust the threshold as needed
+                similarity_scores[filename] = mse
 
-    sim_list = logo_feat_list @ img_feat.T
-    pred_brand_list = file_name_list
-
-    assert len(sim_list) == len(pred_brand_list)
-
-    idx = np.argsort(sim_list)[::-1][:3]
-    pred_brand_list = np.array(pred_brand_list)[idx]
-    sim_list = np.array(sim_list)[idx]
-
-    top3_logolist = [Image.open(x) for x in pred_brand_list]
-    top3_brandlist = [os.path.basename(os.path.dirname(x)) for x in pred_brand_list]
-    top3_domainlist = [domain_map[x] for x in top3_brandlist]
-    top3_simlist = sim_list
-
-    for j in range(3):
-        predicted_brand, predicted_domain = None, None
-
-        if top3_brandlist[j] != top3_brandlist[0]:
-            continue
-
-        if top3_simlist[j] >= t_s:
-            predicted_brand = top3_brandlist[j]
-            predicted_domain = top3_domainlist[j]
-            final_sim = top3_simlist[j]
-        else:
-            cropped, candidate_logo = resolution_alignment(cropped, top3_logolist[j])
-            img_feat = pred_siamese(cropped, model, grayscale=grayscale)
-            logo_feat = pred_siamese(candidate_logo, model, grayscale=grayscale)
-            final_sim = logo_feat.dot(img_feat)
-            if final_sim >= t_s:
-                predicted_brand = top3_brandlist[j]
-                predicted_domain = top3_domainlist[j]
-            else:
-                break
-
-        if predicted_brand is not None:
-            ratio_crop = cropped.size[0] / cropped.size[1]
-            ratio_logo = top3_logolist[j].size[0] / top3_logolist[j].size[1]
-            if max(ratio_crop, ratio_logo) / min(ratio_crop, ratio_logo) > 2.5:
-                continue
-            else:
-                return predicted_brand, predicted_domain, final_sim
-
-    return None, None, top3_simlist[0]
-
-# Example usage:
-# Define your Siamese model, domain_map, logo_feat_list, file_name_list, shot_path, gt_bbox, t_s, and grayscale.
-# Then call siamese_inference to get the predicted brand and domain.
+    return {k: v for k, v in similarity_scores.items() if v > 0.8}
